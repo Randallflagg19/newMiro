7a73b06d72c799e96addfaa71a8b5a8d16fed353
Aleksey Astashonok <lextapir191919@gmail.com>
2025-12-03 11:39:22 +0300

    add prettier & eslint & boundaries"
    "

---
+++ code/src/app/app.tsx
@@ -0,0 +1,11 @@
+import { BoardPage } from "@/features/board";
+import { BoardCard } from "@/features/boards-list";
+
+export function App() {
+  return (
+    <div>
+      <BoardPage />
+      <BoardCard />
+    </div>
+  );
+}
